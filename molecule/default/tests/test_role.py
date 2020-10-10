d7cf85d3a30461acdbbbb778c2d5376d1f4439a1
John Freeman <john.freeman.opensource@gmail.com>
2020-10-10 19:55:23 +0100

    Added support for Python 3 (#24)
    Python 2 is no longer supported.

--- molecule/default/tests/test_role.py
+++ molecule/default/tests/test_role.py
@@ -10,6 +10,7 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 
 def test_version(host):
     version = host.check_output('sudo --set-home --user test_usr1 ' +
-                                'bash --login -c "pipenv --version"')
+                                'bash --login -c "export LC_ALL=C.UTF-8; ' +
+                                'export LANG=C.UTF-8; pipenv --version"')
     pattern = r'version [0-9\.]+'
     assert re.search(pattern, version)
